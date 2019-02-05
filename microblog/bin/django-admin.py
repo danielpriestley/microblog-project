12de528fb7c4e6ec31055a73561088fc9ec801ab
Daniel Priestley <dannypriestley3@gmail.com>
2019-02-05 19:50:27 +0000

    My MicroBlog App, first commit

---
+++ microblog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/danielpriestley/Development/Projects/MicroBlog/microblog/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
