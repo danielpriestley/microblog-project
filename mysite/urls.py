eec347733737632a110a40c36922621b7f2daebd
danielpriestley <dannypriestley3@gmail.com>
2019-02-06 00:57:52 +0000

    trying to fix attributeerror

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from blog.urls import path
-from blog import views
 from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import include, path
+
+# from blog import views
+# from blog.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('blog/', include('blog.urls')),
+    url('', include('blog.urls')),
 ]
