4a6c13f65e2c267d025561708f1326788b834c61
danielpriestley <dannypriestley3@gmail.com>
2019-02-06 00:31:10 +0000

    attempt to fix urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from blog.urls import path
 from blog import views
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.post_list),
+    url('blog/', include('blog.urls')),
 ]
